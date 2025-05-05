54a06a540d7f54201e53eba60dfc178c09deed53
Paul Quevedo <PaulQuevedo@jdrfelectromag.com>
2025-05-05 13:02:13 -0400

    Add initial configuration

---
+++ colors/pquevedo.vim
@@ -0,0 +1,149 @@
+runtime colors/default.vim
+
+highlight SpecialKey ctermfg=red guifg=red  " Set the color to red (or choose another)
+
+syn match Todo /FIXME/
+syn match Todo /TODO/
+
+hi User1 term=NONE ctermfg=yellow       ctermbg=darkgrey
+hi User2 term=NONE ctermfg=green        ctermbg=darkgrey
+hi User3 term=NONE ctermfg=Blue         ctermbg=darkgrey
+hi User4 term=NONE ctermfg=cyan         ctermbg=darkgrey
+hi User5 term=NONE ctermfg=yellow       ctermbg=darkgrey
+hi User6 term=NONE ctermfg=red          ctermbg=darkgrey
+
+hi CursorLine gui=underline cterm=underline
+hi Directory term=NONE ctermfg=LightCyan
+hi Comment term=NONE ctermfg=LightGreen
+hi Constant term=NONE ctermfg=Magenta
+hi String term=NONE ctermfg=DarkRed
+hi Chracter term=NONE ctermfg=Yellow
+hi Number term=NONE ctermfg=Yellow
+hi Boolean term=NONE ctermfg=Yellow
+hi Float term=NONE ctermfg=Yellow
+"hi Identifier term=NONE ctermfg=Magenta
+hi Function term=NONE ctermfg=Magenta
+hi Statement term=NONE ctermfg=red
+hi Conditional term=NONE ctermfg=Magenta
+hi Repeat term=NONE ctermfg=White
+hi Label term=NONE ctermfg=White
+hi Operator term=NONE ctermfg=LightCyan
+hi Keyword term=NONE ctermfg=White
+hi Exception term=NONE ctermfg=White
+hi PreProc term=NONE ctermfg=Gray
+hi Include term=NONE ctermfg=White
+hi Define term=NONE ctermfg=LightCyan
+hi Macro term=NONE ctermfg=Yellow
+hi PreCondit term=NONE ctermfg=LightCyan
+hi Type term=NONE ctermfg=LightCyan
+hi StorageClass term=NONE ctermfg=Cyan
+hi Structure term=NONE ctermfg=LightCyan
+hi Typedef term=NONE ctermfg=LightCyan
+hi Special term=NONE ctermfg=White
+hi SpecialChar term=NONE ctermfg=Cyan
+hi Tag term=NONE ctermfg=Cyan
+hi Delimiter term=NONE ctermfg=Blue
+hi SpecialComment term=NONE ctermfg=LightGreen
+hi Debug term=NONE ctermfg=Red
+hi Ignore term=NONE ctermfg=Red
+hi Error term=NONE ctermfg=Red
+hi Todo term=bold ctermbg=yellow
+
+" Folding Highlights disabled
+highlight Folded     ctermbg=none ctermfg=none
+highlight FoldColumn ctermbg=none ctermfg=none
+
+" Global highlighting overrides
+highlight Comment      ctermfg=grey
+highlight Constant     ctermfg=white
+highlight Search       ctermfg=darkgrey     ctermbg=yellow  term=bold
+highlight Special      ctermfg=cyan
+highlight StatusLine   ctermfg=darkgrey     ctermbg=green   term=none
+highlight StatusLineNC ctermfg=darkgrey     ctermbg=darkred term=none
+highlight VertSplit    ctermfg=darkgrey     ctermbg=darkgrey
+highlight ErrorMsg     ctermfg=red          ctermbg=black
+highlight Error        ctermfg=red          ctermbg=black
+highlight WarningMsg   ctermfg=yellow       cterm=standout
+highlight SpellBad     ctermfg=red          ctermbg=black       cterm=underline
+
+" C syntax highlighting overrides
+highlight cDefine      ctermfg=yellow
+highlight cInclude     ctermfg=white
+highlight cFormat      ctermfg=cyan
+highlight cType        ctermfg=cyan
+highlight cPreCondit   ctermfg=blue
+highlight cSpecial     ctermfg=red
+highlight cNumber      ctermfg=yellow
+highlight cConstant    ctermfg=magenta
+"highlight cFloat       ctermfg=magenta
+highlight cCharacter   ctermfg=blue
+highlight cString      ctermfg=darkred
+highlight cStatement   ctermfg=red
+highlight cComment     ctermfg=green
+
+function! s:pquevedo_AddCCodeHighlighting()
+    " Highlight C typedefs and structs (_t, _s)
+    syntax match cType /\w\+_t\ze\W/
+    syntax match cType /\w\+_s\ze\W/
+
+    syntax match ifType /\w\+IF_t/
+    highlight ifType ctermfg=yellow
+
+    syntax match ctxType /\w\+Ctx_t/
+    highlight ctxType ctermfg=magenta
+
+    syntax match pointer /\*[a-z,A-Z,0-9]*++/
+    highlight pointer ctermfg=darkblue
+endfunction()
+
+
+highlight LogError   ctermfg=darkred term=bold
+highlight LogWarning ctermfg=yellow term=bold
+highlight LogNotice  ctermfg=darkgreen term=bold
+highlight LogDebug   ctermfg=blue
+highlight LogTrace   ctermfg=grey
+
+highlight LosFound    ctermfg=white   ctermbg=darkgreen
+highlight LosNotFound ctermfg=black   ctermbg=yellow
+highlight discoverOn ctermfg=magenta
+
+
+function! s:pquevedo_AddLogHighlighting()
+    " Logging Severity Level Highlighting
+    "   Apply to any unknown filetype
+    syntax match LogError /.*\[\v(ERROR|error)\].*/
+    syntax match LogWarning /.*\[\v(WARN|WARNING|warn|warning)\].*/
+    syntax match LogNotice /.*\[\v(NOTICE|notice)\].*/
+    syntax match LogDebug /.*\[\v(DEBUG|DEBG|DBG|debug|dbg)\].*/
+    syntax match LogTrace /.*\[\v(TRACE|trace)\].*/
+    syntax match LosFound /.*LOS  Found.*/
+    syntax match LosNotFound /.*LOS !Found.*/
+    syntax match discoverOn /.*discoverEnb.*/
+endfunction()
+
+function! UpdateHighlighting() abort
+    let l:ft = &filetype
+    let c_types = ['c','h','cpp','hpp']
+    let log_types = ['log','txt', '']
+    if index(c_types, l:ft) >= 0 || index(log_types, l:ft) >= 0
+        silent! TSDisable highlight
+    else
+        silent! TSEnable highlight
+    endif
+
+    if index(c_types, &l:ft) >= 0
+        syntax on
+        call s:pquevedo_AddCCodeHighlighting()
+    elseif index(log_types, &l:ft) >= 0
+        syntax off
+        call s:pquevedo_AddLogHighlighting()
+    else
+        syntax on
+    endif
+endfunction
+
+augroup CustomHighlighting
+  autocmd!
+  autocmd BufWinEnter,WinEnter * call UpdateHighlighting()
+augroup END
+
---
+++ lua/config/settings.lua
@@ -0,0 +1,88 @@
+local global = vim.g
+local o = vim.opt
+
+vim.cmd.colorscheme('pquevedo') -- ~/.config/nvim/colors
+
+-- Editor options
+-------------------------------------------------------------------------------------------------------
+o.number = false -- Print the line number in front of each line
+o.relativenumber = false -- Show the line number relative to the line with the cursor in front of each line.
+o.clipboard = "unnamedplus" -- uses the clipboard register for all operations except yank.
+o.syntax = "on" -- When this option is set, the syntax with this name is loaded.
+o.autoindent = true -- Copy indent from current line when starting a new line.
+o.cursorline = false -- Highlight the screen line of the cursor with CursorLine.
+o.expandtab = true -- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
+o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+o.shiftround = true -- When on, the indent of a line is changed to the indent of the line above it.
+o.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
+o.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing.
+o.smarttab = true -- When on, <Tab> and <BS> will use the same number of spaces as 'shiftwidth' for indenting.
+o.encoding = "UTF-8" -- Sets the character encoding used inside Vim.
+o.ruler = true -- Show the line and column number of the cursor position, separated by a comma.
+o.mouse = "" -- If "a" then the mouse can be used to enter visual mode but you loose the ability to highlight text to copy so use "" instead
+o.title = true -- When on, the title of the window will be set to the value of 'titlestring'
+o.hidden = true -- When on a buffer becomes hidden when it is |abandon|ed
+o.ttimeoutlen = 0 -- The time in milliseconds that is waited for a key code or mapped key sequence to complete.
+o.wildmenu = true -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
+o.showcmd = true -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
+o.showmatch = true -- When a bracket is inserted, briefly jump to the matching one.
+o.inccommand = "split" -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
+o.splitright = true
+o.splitbelow = true -- When on, splitting a window will put the new window below the current one
+o.termguicolors = false -- Disable 24-bit RGB color in the TUI
+o.wildmode = "longest,list,full" -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
+o.ignorecase = true -- When on, case is ignored in file names and search patterns
+o.smartcase = true -- unless the search pattern contains upper case characters. In that case, the search is case sensitive.
+o.wrap = false -- When on, long lines are not wrapped and can be scrolled horizontally.
+o.laststatus = 2 -- When set to 2, the last window will always have a status line.
+o.tags = "./tags;/" -- This will look in the current directory for <tags>, and work up the tree towards root until one is found.
+o.incsearch = true -- When on, the search command will show matches for each typed character.
+o.backup = true -- When on, a backup file is created when a file is edited.
+o.backspace = { "indent", "eol", "start" } -- This option controls how Vim handles backspacing over autoindent, line breaks and start of insert.
+o.hlsearch = true -- When on, all search patterns are highlighted.
+o.numberwidth = 6 -- Set the width of the number column to 6 characters
+o.list = true -- Show whitespace characters
+o.listchars = { tab = "▸ ", trail = "·" } -- Show whitespace characters
+
+-- Keymaps
+-------------------------------------------------------------------------------------------------------
+vim.keymap.set('n', '<F12>', ':vsplit<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<F9>", ":setlocal spell! spelllang=en_us<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F3>", ":set invnumber<CR> :set invcursorline<CR>", { noremap = true, silent = true })
+
+-- Insert a divider comment line
+vim.keymap.set('i', '<C-l>', '/******************************************************************************/', { noremap = true })
+-- Insert a block header comment
+vim.keymap.set('i', '<C-b>', '/*******************************************************************************\n\x0d\x08\x08\x08 *\n\x08\x08\x08 *******************************************************************************/', { noremap = true })
+
+
+-- Map ctrl+j to accept a copilot suggestion; disable use of tab to accept
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.keymap.set('i', '<M-]>', '<Plug>(copilot-next)') -- ALT+] go to next suggestion
+vim.keymap.set('i', '<M-[>', '<Plug>(copilot-previous)') -- ALT+[ go to previous suggestion
+
+-- Autocommands
+-------------------------------------------------------------------------------------------------------
+
+-- When opening a file jump to the last known cursor position (if available)
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
+
+-- Remove trailing whitespace for each line on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
