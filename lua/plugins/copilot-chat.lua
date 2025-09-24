84792471bc929b22bb624b037aa5e0bb50468552
Paul Quevedo <PaulQuevedo@jdrfelectromag.com>
2025-09-24 09:50:34 -0400

    Steal Robs CopilotChat settings; use claude-sonnet-4 as the model

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -12,7 +12,32 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      -- See Configuration section for options
+        -- model = 'gpt-4.1', -- AI model to use
+        model = 'claude-sonnet-4',
+        temperature = 0.1, -- Lower = focused, higher = creative
+        auto_insert_mode = true,
+
+        headers = {
+            user = '👤 You',
+            assistant = '🤖 Copilot',
+            tool = '🔧 Tool',
+        },
+
+        separator = '━━',
+        auto_fold = true, -- Automatically folds non-assistant messages
+
+        prompts = {
+            PyTest = {
+                prompt = "#buffers:all\n" ..
+                         "Write unit-tests for this code using the unittest framework\n" ..
+                         "Ensure at least 80% code coverage"
+            },
+            GTest = {
+                prompt = "#buffers:all\n" ..
+                         "Write unit-tests for this code using the googletest framework\n" ..
+                         "Ensure at least 80% code coverage"
+            }
+        }
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
